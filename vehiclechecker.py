23e825e3014be77858e2ad9092a29186bb693a6b
mirak2010 <mirakhmad0101@yahoo.com>
2025-01-26 10:44:37 +0500

    #

---
+++ vehiclechecker.py
@@ -0,0 +1,16 @@
+choice=input("Please enter your choice for a ride(car or bike): ")
+if choice=="car":
+    x=input("Choose the type of car(suv or sedan): ")
+    if x=="suv":
+        print("You are big guy")
+    if x=="sedan":
+            print("You are a small guy")
+
+
+if choice=="bike":
+    y=input("Please enter the type of the bike(scooter or sport): ")
+    if y=="scooter":
+        print("You are a safe guy")
+    if y=="sport":
+        print("You are a dangerous guy!")
+
